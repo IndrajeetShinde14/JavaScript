c59c3d76f1378711e6cb9e37cc26d1aa9558d0b7
Indrajeet Shinde <106182270+IndrajeetShinde14@users.noreply.github.com>
2024-01-17 19:10:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)  //we are using NodeJS, not browser
+
+let name = "Indrajeet"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string  => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object 
+
+
+console.log(typeof "Indrajeet");
+console.log(typeof null);
+console.log(typeof undefined);
