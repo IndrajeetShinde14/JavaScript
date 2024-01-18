1e7bfcc0cb88a61c45f2983a88137a7628d67ca0
Indrajeet Shinde <106182270+IndrajeetShinde14@users.noreply.github.com>
2024-01-18 05:11:23 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33abc"
 
-//console.log(typeof score);
-//console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 // console.log(typeof (valueInNumber));
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Indrajeet"
 
 let booleanIsLoggesIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggesIn);
+// console.log(booleanIsLoggesIn);
 
 // 1=> true; 0 => false
 //"" => false
@@ -24,5 +24,38 @@ console.log(booleanIsLoggesIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ********************* Operations ********************* 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Indrajeet"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter  = 100
+gameCounter++;
+console.log(gameCounter);
